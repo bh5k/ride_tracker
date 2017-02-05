e9425bbb9352f31e9583d3a1574769d44d7cf8a9
Bryan Hansen <bryankhansen@gmail.com>
2017-02-05 11:51:08 -0700

    Added test structure and logging
    Incorporated Log4j and a richer test client

---
+++ src/main/java/com/pluralsight/model/Ride.java
@@ -0,0 +1,24 @@
+package com.pluralsight.model;
+
+public class Ride {
+
+	private String name;
+	private int duration;
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -0,0 +1,30 @@
+package com.pluralsight.controller;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.pluralsight.model.Ride;
+
+import org.junit.Test;
+
+public class RestControllerTest {
+
+	@Test(timeout=1000)
+	public void testGetRides() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
+				"http://localhost:8080/ride_tracker/rides", HttpMethod.GET,
+				null, new ParameterizedTypeReference<List<Ride>>() {
+				});
+		List<Ride> rides = ridesResponse.getBody();
+
+		for (Ride ride : rides) {
+			System.out.println("Ride name: " + ride.getName());
+		}
+	}
+}

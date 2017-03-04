b3b4f6ec2dc0032e70b172cbaf5fab33cc564211
Bryan Hansen <bryankhansen@gmail.com>
2017-03-04 14:40:34 -0700

    Module 3 final code
    Ride Controller method added to create a ride. Create Ride added in
    repository and service tier, unit test created, alternate approach with
    SimpleJdbcInsert shown.

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -14,6 +17,31 @@ public class RideRepositoryImpl implements RideRepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public Ride createRide(Ride ride) {		
+		//jdbcTemplate.update("insert into ride (name, duration) values (?,?)", ride.getName(), ride.getDuration());
+		
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		
+		insert.setGeneratedKeyName("id");
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", ride.getName());
+		data.put("duration", ride.getDuration());
+		
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+		
+		insert.setTableName("ride");
+		insert.setColumnNames(columns);
+		Number key = insert.executeAndReturnKey(data);
+		
+		System.out.println(key);
+		
+		return null;
+	}
 	
 	@Override
 	public List<Ride> getRides() {
